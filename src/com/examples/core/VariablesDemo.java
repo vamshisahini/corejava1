035a08432592223ac2d22b196a8d8753718187d7
vamshi reddy <vamsireddy96180@gmail.com>
2023-05-16 16:00:50 +0530

    initial project commit

---
+++ src/com/examples/core/VariablesDemo.java
@@ -0,0 +1,11 @@
+package com.examples.core;
+
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int firstNumber=35;
+        int secondNumber=6;
+        double result;
+        result =  (double)firstNumber / secondNumber;
+        System.out.println("result = " + result);
+    }
+}
